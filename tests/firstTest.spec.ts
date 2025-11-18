009514703d9f31badbfab4e75b07a0b40b06e68f
KateGordin <kate.gordin@gmail.com>
2025-11-18 14:26:45 +0100

    Refactor first test to use beforeEach for setup and add new locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,14 +1,36 @@
 import { test } from '@playwright/test';
 
-test('the first test', async ({page}) => {
-await page.goto('http://localhost:55295/')
-await page.getByText('Forms'). click()
-await page.getByText('Form Layouts'). click()
+test.beforeEach(async ({page}) => {
+  await page.goto('http://localhost:4200/')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
 })
 
-test('navigate to datepicker page', async ({page}) => {
-  await page.goto('http://localhost:55295/')
-  await page.getByText('Forms'). click()
-  await page.getByText('Datepicker'). click()
-  })
+test('Locator syntax rules', async ({page}) => {
+  // by Tag name
+  await page.locator('input').first().click()
 
+  // by ID
+  page.locator('#inputEmail1')
+
+  // by Class value
+  page.locator('.shape-rectangle')
+
+  // by attribute
+  page.locator('[placeholder="Email"]')
+
+  // by Class value (full)
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+  //combina different selectors
+  page.locator('input[placeholder="Email"][nbinput]')
+
+  // by XPath (NOT RECOMMENDED)
+  page.locator('//*[@id="inputEmail1"]')
+
+  // by partial text match
+  page.locator(':text=("Using")')
+
+  // by exact text match
+  page.locator(':text-is("Using the Grid")')
+})
