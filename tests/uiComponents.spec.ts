14121053b78dc25cd5870457710da82ef2d8e93c
KateGordin <kate.gordin@gmail.com>
2025-11-20 16:38:19 +0100

    Radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -24,4 +24,23 @@ test.describe('Forms Layouts page', () => {
     // locator assertion
     await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
   })
+
+  test('radio buttons', async ({page}) => {
+    const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+    // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+    await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true})
+    const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
+
+    // generic assertion
+    expect(radioStatus).toBeTruthy()
+
+    // locator assertion
+    await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked()
+
+    await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force: true})
+    // generic assertion
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
+  })
 })
